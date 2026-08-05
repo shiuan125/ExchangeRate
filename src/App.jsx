5952d62508d98329261f076f08a838b155d1d768
shiuan125 <shiuan125@gmail.com>
2026-08-05 14:11:51 +0800

    feat: display app version in footer (v1.1.1)
    - package.json version 改為 1.1.1
    - 新增 Footer 元件，從 package.json 讀版本號，顯示在頁面最下面

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { LiveRatePanel } from './components/LiveRatePanel';
 import { HistoryChart } from './components/HistoryChart';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="page">
       <LiveRatePanel />
       <HistoryChart />
+      <Footer />
     </div>
   );
 }
